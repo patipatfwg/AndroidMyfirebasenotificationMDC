3a5aa4faf76eeb02bce338cbc863e30a95fdd240
Patipat Chanrungruang <patipat_cha@freewillsolutions.com>
2020-09-24 13:38:02 +0700

    first

---
+++ app/src/main/java/com/cuez/myfirebasenotification/MainActivity.java
@@ -0,0 +1,171 @@
+package fwg.mdc.btc.cuez_diabeetes_android;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+
+
+public class MainActivity extends AppCompatActivity {
+	private static final String AUTH_KEY_SOSO = "key=AAAAo8_P4_I:APA91bFO_V-fOT1XNT8ibZ00b33UPQ8mTySgXNGvq5XszxbckySv3EH0YcURVSRRMrB03fUv5ZJuqrvaMZ5NeMu8N4VgUoxdF0nY4QHGBZ63y79mbualGsQrPblcyPZYO5N6O13PG6TA";
+	private static final String AUTH_KEY_CUEZ = "key=AAAApNxyi24:APA91bHBgO8dsG_F16nyO-KK2YEFGU4l1MZKF7kuo7JeZlNXuZD3uV2QjcgXh6Mr5NPlkV7KViWxjNN7B0edzQVXC-1VcSV9xkxOExnvKjdlfqZrFFg5WKH7OWwEnKMbzeb_v6gQnAWR";
+
+	private TextView mTextView;
+	private String token;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mTextView = findViewById(R.id.txt);
+
+		Bundle bundle = getIntent().getExtras();
+		if (bundle != null) {
+			String tmp = "";
+			for (String key : bundle.keySet()) {
+				Object value = bundle.get(key);
+				tmp += key + ": " + value + "\n\n";
+			}
+			mTextView.setText(tmp);
+		}
+
+		FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+			@Override
+			public void onComplete(@NonNull Task<InstanceIdResult> task) {
+				if (!task.isSuccessful()) {
+					token = task.getException().getMessage();
+					Log.w("FCM TOKEN Failed", task.getException());
+				} else {
+					token = task.getResult().getToken();
+					Log.i("FCM TOKEN ==> ", token);
+				}
+			}
+		});
+	}
+
+	public void showToken(View view) {
+		mTextView.setText(token);
+	}
+
+	public void subscribe(View view) {
+		FirebaseMessaging.getInstance().subscribeToTopic("news");
+		mTextView.setText(R.string.subscribed);
+	}
+
+	public void unsubscribe(View view) {
+		FirebaseMessaging.getInstance().unsubscribeFromTopic("news");
+		mTextView.setText(R.string.unsubscribed);
+	}
+
+	public void sendToken(View view) {
+		sendWithOtherThread("token");
+	}
+
+	public void sendTokens(View view) {
+		sendWithOtherThread("tokens");
+	}
+
+	public void sendTopic(View view) {
+		sendWithOtherThread("topic");
+	}
+
+	private void sendWithOtherThread(final String type) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				pushNotification(type);
+			}
+		}).start();
+	}
+
+	private void pushNotification(String type) {
+		JSONObject jPayload = new JSONObject();
+		JSONObject jNotification = new JSONObject();
+		JSONObject jData = new JSONObject();
+		try {
+			jNotification.put("title", "Google I/O 2016");
+			jNotification.put("body", "Firebase Cloud Messaging (App)");
+			jNotification.put("sound", "default");
+			jNotification.put("badge", "1");
+			jNotification.put("click_action", "OPEN_ACTIVITY_1");
+			jNotification.put("icon", "ic_notification");
+
+			jData.put("picture", "https://miro.medium.com/max/1400/1*QyVPcBbT_jENl8TGblk52w.png");
+
+			switch(type) {
+				case "tokens":
+					JSONArray ja = new JSONArray();
+					ja.put("c5pBXXsuCN0:APA91bH8nLMt084KpzMrmSWRS2SnKZudyNjtFVxLRG7VFEFk_RgOm-Q5EQr_oOcLbVcCjFH6vIXIyWhST1jdhR8WMatujccY5uy1TE0hkppW_TSnSBiUsH_tRReutEgsmIMmq8fexTmL");
+					ja.put(token);
+					jPayload.put("registration_ids", ja);
+					break;
+				case "topic":
+					jPayload.put("to", "/topics/news");
+					break;
+				case "condition":
+					jPayload.put("condition", "'sport' in topics || 'news' in topics");
+					break;
+				default:
+					jPayload.put("to", token);
+			}
+
+			jPayload.put("priority", "high");
+			jPayload.put("notification", jNotification);
+			jPayload.put("data", jData);
+
+			URL url = new URL("https://fcm.googleapis.com/fcm/send");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Authorization", AUTH_KEY_CUEZ);
+			conn.setRequestProperty("Content-Type", "application/json");
+			conn.setDoOutput(true);
+
+			// Send FCM message content.
+			OutputStream outputStream = conn.getOutputStream();
+			outputStream.write(jPayload.toString().getBytes());
+
+			// Read FCM response.
+			InputStream inputStream = conn.getInputStream();
+			final String resp = convertStreamToString(inputStream);
+
+			Handler h = new Handler(Looper.getMainLooper());
+			h.post(new Runnable() {
+				@Override
+				public void run() {
+					mTextView.setText(resp);
+				}
+			});
+		} catch (JSONException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String convertStreamToString(InputStream is) {
+		Scanner s = new Scanner(is).useDelimiter("\\A");
+		return s.hasNext() ? s.next().replace(",", ",\n") : "";
+	}
+}
